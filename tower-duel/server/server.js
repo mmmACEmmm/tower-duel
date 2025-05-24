f577dbc9288a7bdec0ce40a4a196b1de18c14159
ACE <acestin.mcintosh@gmail.com>
2025-05-23 22:56:20 -0700

    Delete tower-duel directory

--- tower-duel/server/server.js
+++
@@ -1,7 +0,0 @@
-const io = require('socket.io')(3000);
-io.on('connection', socket => {
-  socket.on('join', room => socket.join(room));
-  socket.on('slice', ({ room, precision }) => {
-    socket.to(room).emit('wobble', { precision });
-  });
-});
